40095adc93fba35c748ae53561a80cf290c199cc
Marek Kukan <marek.kukan@gmail.com>
2017-05-03 17:59:41 +0200

    new feature: graph

--- run_tournament.py
+++ run_tournament.py
@@ -3,6 +3,8 @@
 
 import os
 import sys
+import datetime
+import matplotlib.pyplot as plt
 from player import Player
 from tournament import Tournament
 
@@ -14,6 +16,28 @@ if __name__ == '__main__':
     for p in players:
         p.run()
     tournament = Tournament(players, n_games)
-    tournament.run()
+    wins = tournament.run()
+    players = sorted(players, key=lambda x: x.id)
+
+    ### plot
+    G = len(wins)
+    P = len(players)
+    COLORS = ['0.5', 'purple', 'r', 'g', 'b'] + ['k'] * P
+    c = [0] * P
+    d = [[0] * (G+1) for _ in range(P)]
+    for i,p in enumerate(wins):
+        c[p] += 1
+        for j in range(P):
+            d[j][i+1] = d[j][i]
+        d[p][i+1] += 1
+    for i in range(P):
+        plt.plot(range(G+1), d[i], COLORS[i], linestyle='-')
+        plt.text(G+G//100, d[i][-1], players[i]._path, color=COLORS[i])
+    plt.title(str(datetime.date.today()))
+    plt.xlabel('number of games')
+    plt.ylabel('number of wins')
+    plt.savefig('graph-'+str(datetime.date.today())+'.svg', bbox_inches='tight')
+    ### end plot
+
     print('the end.')
 
--- tournament.py
+++ tournament.py
@@ -13,13 +13,15 @@ class Tournament:
         for i,p in enumerate(self.players):
             p.id = i
             p.write('NEW_TOURNAMENT ' + str(len(self.players)) + ' ' + str(p.id) + ' ' + str(self.n_games))
+        wins = []
         for i in range(self.n_games):
             sys.stderr.write('\r' + str(i+1) + '/' + str(self.n_games))
             random.shuffle(self.players)
             game = Game(self.players)
-            game.run()
+            wins.append(game.run())
         sys.stderr.write('\n')
         print('results:')
         for i,p in enumerate(sorted(self.players, key=lambda x: x.n_wins, reverse=True)):
             print(str(i+1) + '. ' + str(p) + '\t(' + str(p.n_wins) + ' wins)')
+        return wins
 
